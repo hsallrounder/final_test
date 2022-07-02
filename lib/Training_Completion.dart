fc4c8b7b77ad241cf5065503ed6f875a6752a221
Himanshu Sharma <hsallrounder@gmail.com>
2022-07-02 17:35:52 +0530

    Commit

--- lib/Training_Completion.dart
+++ lib/Training_Completion.dart
@@ -8,32 +8,53 @@ class Training_Completion extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text("Hi, ${name}!", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),),
+            Text(
+              "Hi, ${name}!",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 34),
+            ),
             SizedBox(
               height: 30,
             ),
-            Image(image: NetworkImage("https://static.vecteezy.com/system/resources/previews/002/349/754/original/modern-elegant-certificate-template-free-vector.jpg"),),
+            Image(
+              image: NetworkImage(
+                  "https://static.vecteezy.com/system/resources/previews/002/349/754/original/modern-elegant-certificate-template-free-vector.jpg"),
+            ),
             SizedBox(
               height: 20,
             ),
-            Text("You have sucessfully completed Hybrid Mobile App Development Course."),
+            Container(
+              padding: EdgeInsets.all(20),
+              alignment: Alignment.center,
+              child: Text(
+                  "You have sucessfully completed Hybrid Mobile App Development Course."),
+            ),
             SizedBox(
               height: 20,
             ),
-            Container(alignment: Alignment.bottomLeft ,
+            Container(
+                alignment: Alignment.bottomLeft,
                 padding: EdgeInsets.all(20),
                 child: Column(
                   children: [
-                Text("INSTRUCTOR NAME",style: TextStyle(fontWeight: FontWeight.bold),),
-                Text("Pankaj Kapoor", style: TextStyle(color: Colors.deepPurpleAccent),),
-              ],
-            )),
-
+                    Text(
+                      "INSTRUCTOR NAME",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text(
+                      "Pankaj Kapoor",
+                      style: TextStyle(color: Colors.deepPurpleAccent),
+                    ),
+                  ],
+                )),
             Container(
               padding: EdgeInsets.all(20),
               alignment: Alignment.bottomRight,
-              child: Text("Date: ${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}", style: TextStyle(fontWeight: FontWeight.bold),),
+              child: Text(
+                "Date: ${DateTime.now().day}-${DateTime.now().month}-${DateTime.now().year}",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             )
           ],
         ),
